5b6934cca66334a9e0d221e1b523108dfef523df
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-04-24 12:36:54 +0200

    add checkpoint meta to data class (forgor)

--- shared/src/main/kotlin/dev/spaghett/shared/RoomMeta.kt
+++ shared/src/main/kotlin/dev/spaghett/shared/RoomMeta.kt
@@ -1,8 +1,15 @@
 package dev.spaghett.shared
 
+data class Checkpoint(
+    val x: Int,
+    val y: Int,
+    val z: Int
+)
+
 data class RoomMeta(
     val name: String,
     val width: Int, // X
     val height: Int,
-    val depth: Int // Z
+    val depth: Int, // Z
+    val checkpoints: List<Checkpoint>
 )
